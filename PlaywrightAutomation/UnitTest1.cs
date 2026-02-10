06e92418e0a58c1b9d08048f45f2601f30084ced
Vennela629 <svenla26@gmail.com>
2026-02-10 11:22:39 -0500

    updated logintestcase

--- PlaywrightAutomation/UnitTest1.cs
+++ PlaywrightAutomation/UnitTest1.cs
@@ -1,19 +1,42 @@
-﻿using System;
-using NUnit.Framework;
+﻿// using Microsoft.Playwright;
+// using NUnit.Framework;
+// using System;
+// using System.Diagnostics.CodeAnalysis;
+// using System.Threading.Tasks;
 
-namespace PlaywrightAutomation;
+// namespace PlaywrightProject;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+// public class Tests
+// {
+//     [SetUp]
+//     public void Setup()
+//     {
+//     }
 
-    [Test]
-    public void Test1()
-    {
-        Console.WriteLine("Hello World");
-        Assert.Pass();
-    }
-}
+//     [Test]
+//     public async Task Test1()
+//     {
+//         //playwright
+//         using var playwright = await Playwright.CreateAsync();
+
+// //browser
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+// //page
+//       var page = await browser.NewPageAsync();
+//       await page.GotoAsync("http://www.eaapp.somee.com/");
+//       //await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+//        var title =  await page.TitleAsync();
+//          Console.WriteLine(title);
+//          await page.ClickAsync("text=Visit now");
+//         // await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+//         string currentUrl = page.Url;
+//          Console.WriteLine(page.Url);
+//       await page.CloseAsync();
+       
+//         Console.WriteLine("Hello World");
+//         Assert.Pass();
+//     }
+// }
